89d826c855a3651ff47a7396709fcac391054f91
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2023-06-12 22:07:04 +0200

    Corregida clase Pista, siempre ponia la letra i ya que habia que usar equals, y el contructor estaba mal ya que se asignaba el nombre antes de la ubicacion, y para asignar el nombre se necesita la ubicacion.

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -4,6 +4,7 @@ package presentation;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
+import data.PistaRemoteDataSource;
 import domain.models.*;
 import domain.useCase.*;
 
@@ -12,57 +13,52 @@ import domain.useCase.*;
 public class Main {
 	
 	public static void main(String[] args) throws SQLException {
-		
-		AddJugadorUseCase addJugador = new AddJugadorUseCase();
-		DeleteJugadorUseCase deleteJugador = new DeleteJugadorUseCase();
-		SearchJugadorUseCase searchJugador = new SearchJugadorUseCase();
-		
-		AddEquipoUseCase addEquipo = new AddEquipoUseCase();
-		DeleteEquipoUseCase deleteEquipo = new DeleteEquipoUseCase();
-		SearchEquipoUseCase searchEquipo= new SearchEquipoUseCase();
-		
-		Equipo equipo1 = new Equipo(1, "Yepes", Categoria.Senior);
-		
-		Equipo equipo2 = new Equipo(2, "Dioce", Categoria.Senior);
-		
-		addEquipo.execute(equipo1);
-		addEquipo.execute(equipo2);
-		
-		System.out.println(searchEquipo.execute(1));
-		
-		deleteEquipo.execute(2);
-		
-		
-		
-		Jugador jugador = new Jugador();
-		jugador.setId(1);
-		jugador.setNombre("Carlos");
-		jugador.setApellidos("Jimenez Rodrigez");
-		jugador.setEquipo(equipo1);
-		jugador.setFechaNacimiento(LocalDate.of(2000, 1, 1));
-		
-		addJugador.execute(jugador);
-		
-		System.out.println(searchJugador.execute(1).toString());
-		
-		deleteJugador.execute(1);
-		
+		Pista pista = new Pista(domain.models.Pista.Ubicacion.Derecha);
+		AddPistaUseCase addPistaUseCase = new AddPistaUseCase();
+		addPistaUseCase.execute(pista);
+
+//		AddJugadorUseCase addJugador = new AddJugadorUseCase();
+//		DeleteJugadorUseCase deleteJugador = new DeleteJugadorUseCase();
+//		SearchJugadorUseCase searchJugador = new SearchJugadorUseCase();
+//
+//		AddEquipoUseCase addEquipo = new AddEquipoUseCase();
+//		DeleteEquipoUseCase deleteEquipo = new DeleteEquipoUseCase();
+//		SearchEquipoUseCase searchEquipo= new SearchEquipoUseCase();
+//
+//		Equipo equipo1 = new Equipo(1, "Yepes", Categoria.Senior);
+//
+//		Equipo equipo2 = new Equipo(2, "Dioce", Categoria.Senior);
+//
+//		addEquipo.execute(equipo1);
+//		addEquipo.execute(equipo2);
+//
+//		System.out.println(searchEquipo.execute(1));
+//
+//		deleteEquipo.execute(2);
+//
+//
+//
+//		Jugador jugador = new Jugador();
+//		jugador.setId(14);
+//		jugador.setNombre("Carlos");
+//		jugador.setApellidos("Jimenez Rodrigez");
+//		jugador.setEquipo(equipo1);
+//		jugador.setFechaNacimiento(LocalDate.of(2000, 1, 1));
+//
+//		addJugador.execute(jugador);
+//
+//		System.out.println(searchJugador.execute(14).toString());
+//
+//		deleteJugador.execute(1);
+//
 //		jugador.setFechaNacimiento(LocalDate.of(2000, 5, 5));
-//		
+//
 //		System.out.println(jugador);
-//		
+//
 //		Arbitro arbitro = new Arbitro(1, Categoria.Senior);
-//		
-//		Pista pista = new Pista(domain.models.Pista.Ubicacion.Derecha);
-//		
-//		Partido partido = new Partido();
-//		partido.setId(1);
-//		partido.setPista(pista);
-//		partido.setEquipo1(equipo1);
-//		partido.setEquipo2(equipo2);
-//		partido.setArbitro(arbitro);
-//		partido.setResultado("100-99");
-		
+
+
+
 		
 		
 	}
